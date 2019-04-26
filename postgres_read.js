31ceb67a0e4f4f5e40185ece6f366dc6fbb7d3cf
Kazuki Shigemitsu <kazuki.shigemitsu@gmail.com>
2019-04-25 19:56:33 -0700

    updated the query

--- postgres_read.js
+++ postgres_read.js
@@ -1,12 +1,18 @@
-const { Pool, Client } = require('pg');
+const { Pool } = require('pg');
 
 process.env.PGHOST = "localhost";
 process.env.PGDATABASE = "node_db";
 process.env.PGUSER = "node_user";
 process.env.PGPASSWORD = "node_123";
 
+
 const pool = new Pool()
-query = "select * from albums where id = 10000000"
+
+const query = "select * from albums where id = 10000000 limit 1";
+
+startTime = (new Date()).getTime()
 pool.query(query, function(err, res) {
   pool.end()
+  endTime = (new Date()).getTime()
+  console.log((endTime-startTime) + " ms");
 });
